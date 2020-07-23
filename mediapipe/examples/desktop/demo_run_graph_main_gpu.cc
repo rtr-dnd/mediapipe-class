973ba026b14dd54909aed38903f84def2223ba93
rtr-dnd <skysoyn@gmail.com>
2020-07-23 18:06:35 +0900

    addedcomment

--- mediapipe/examples/desktop/demo_run_graph_main_gpu.cc
+++ mediapipe/examples/desktop/demo_run_graph_main_gpu.cc
@@ -36,9 +36,9 @@
 #include "mediapipe/framework/formats/rect.pb.h"
 
 constexpr char kInputStream[] = "input_video";
-constexpr char kOutputStream[] = "output_video";
-constexpr char lOutputStream[] = "output_landmarks";
-constexpr char rOutputStream[] = "output_hand_rect";
+constexpr char kOutputStream[] = "output_video"; // MediaPipeから出力された映像
+constexpr char lOutputStream[] = "output_landmarks"; // MediaPipeから出力された手の特徴量情報
+constexpr char rOutputStream[] = "output_hand_rect"; // MediaPipeから出力された、映像内での手の位置
 constexpr char kWindowName[] = "MediaPipe";
 
 DEFINE_string(
@@ -51,6 +51,7 @@ DEFINE_string(output_video_path, "",
               "Full path of where to save result (.mp4 only). "
               "If not provided, show result in a window.");
 
+// アルファ値を保持したまま画像を別の画像の上に描画する
 void overlayImage(cv::Mat* src, cv::Mat* overlay, const cv::Point& location) {
     for (int y = std::max(location.y, 0); y < src->rows; ++y) {
         int fY = y - location.y;
@@ -71,7 +72,7 @@ void overlayImage(cv::Mat* src, cv::Mat* overlay, const cv::Point& location) {
 }
 
 ::mediapipe::Status RunMPPGraph() {
-  const bool pass_through = false;
+  const bool pass_through = false; // パススルーモードのオンオフ
 
   std::string calculator_graph_config_contents;
   MP_RETURN_IF_ERROR(mediapipe::file::GetContents(
@@ -98,8 +99,12 @@ void overlayImage(cv::Mat* src, cv::Mat* overlay, const cv::Point& location) {
   if (load_video) {
     capture.open(FLAGS_input_video_path);
   } else {
-    // capture.open(0); // Integrated Camera
-    capture.open(0); // webcam
+    if (pass_through)
+    {
+      capture.open(1); // ウェブカム
+    } else {
+      capture.open(0); // 内蔵カメラ
+    }
   }
   RET_CHECK(capture.isOpened());
 
@@ -115,6 +120,7 @@ void overlayImage(cv::Mat* src, cv::Mat* overlay, const cv::Point& location) {
   }
 
   LOG(INFO) << "Start running the calculator graph.";
+  // それぞれのストリームのポーラを設定
   ASSIGN_OR_RETURN(mediapipe::OutputStreamPoller poller,
                    graph.AddOutputStreamPoller(kOutputStream));
   ASSIGN_OR_RETURN(mediapipe::OutputStreamPoller poller2,
@@ -125,13 +131,19 @@ void overlayImage(cv::Mat* src, cv::Mat* overlay, const cv::Point& location) {
 
   LOG(INFO) << "Start grabbing and processing frames.";
   bool grab_frames = true;
+
+  // 今ピンチ中か否か
   bool isActive = false;
+
+  // 直近aveNum個の距離と中点の値を保存しておき、平均を取って描画する
   const int aveNum = 5;
   float recentDistance[aveNum] = {};
   float recentBetweenVX[aveNum] = {};
   float recentBetweenVY[aveNum] = {};
-  // count failure and stop showing window when reached threshold
+
+  // ピンチを検出できないと増える。これが一定以下なら、さっきまでピンチしていたということなので、次のフレームで指の距離が離れていても、ピンチアウト途中だと判定
   int failCount = 0;
+
   while (grab_frames) {
     // Capture opencv camera or video frame.
     cv::Mat camera_frame_raw;
@@ -139,8 +151,8 @@ void overlayImage(cv::Mat* src, cv::Mat* overlay, const cv::Point& location) {
     if (camera_frame_raw.empty()) break;  // End of video.
     cv::Mat camera_frame;
     cv::cvtColor(camera_frame_raw, camera_frame, cv::COLOR_BGR2RGB);
-    // if (!load_video) {
     if(!pass_through) { 
+      // ミラーモードなら映像を反転
       cv::flip(camera_frame, camera_frame, /*flipcode=HORIZONTAL*/ 1);
     }
 
@@ -170,6 +182,7 @@ void overlayImage(cv::Mat* src, cv::Mat* overlay, const cv::Point& location) {
         }));
 
     // Get the graph result packet, or stop if that fails.
+    // MediaPipeのパケットを取得
     mediapipe::Packet packet;
     mediapipe::Packet packet2;
     mediapipe::Packet packet3;
@@ -178,16 +191,16 @@ void overlayImage(cv::Mat* src, cv::Mat* overlay, const cv::Point& location) {
     if (!poller3.Next(&packet3)) break;
     std::unique_ptr<mediapipe::ImageFrame> output_frame;
 
-    // display packet2
+    // パケットから各特徴量を取得
     mediapipe::NormalizedLandmarkList landmarkList = packet2.Get<mediapipe::NormalizedLandmarkList>();
     mediapipe::NormalizedLandmark thumbTip = landmarkList.landmark(4);
     mediapipe::NormalizedLandmark indexTip = landmarkList.landmark(8);
 
     // generating vectors for hand posture detection
-    const std::vector<float> indexBottomV = {
+    const std::vector<float> indexBottomV = { // 人差し指付け根のベクトル
       landmarkList.landmark(6).x() - landmarkList.landmark(5).x(),
       landmarkList.landmark(6).y() - landmarkList.landmark(5).y()};
-    const std::vector<float> middleTipV = {
+    const std::vector<float> middleTipV = { // 中指先端のベクトル、以下同様
       landmarkList.landmark(12).x() - landmarkList.landmark(11).x(),
       landmarkList.landmark(12).y() - landmarkList.landmark(11).y()};
     const std::vector<float> ringTipV = {
@@ -197,25 +210,25 @@ void overlayImage(cv::Mat* src, cv::Mat* overlay, const cv::Point& location) {
       landmarkList.landmark(20).x() - landmarkList.landmark(19).x(),
       landmarkList.landmark(20).y() - landmarkList.landmark(19).y()};
 
-    const std::vector<float> thumbDirV = {
+    const std::vector<float> thumbDirV = { // 人差し指全体のベクトル
       landmarkList.landmark(4).x() - landmarkList.landmark(3).x(),
       landmarkList.landmark(4).y() - landmarkList.landmark(3).y()};
     const std::vector<float> indexDirV = {
       landmarkList.landmark(8).x() - landmarkList.landmark(7).x(),
       landmarkList.landmark(8).y() - landmarkList.landmark(7).y()};
-    std::vector<float> betweenDirV = {0, 0};
-    if(pass_through) {
+    std::vector<float> betweenDirV = {0, 0}; // 親指と人差し指の中点の向きのベクトル
+    if(pass_through) { // パススルーなら中点そのまま
       betweenDirV[0] = (thumbDirV[0] + indexDirV[0])/2; 
       betweenDirV[1] = (thumbDirV[1] + indexDirV[1])/2; 
-    } else {
+    } else { // ミラーなら重み付き平均
       betweenDirV[0] = (thumbDirV[0]*9 + indexDirV[0])/10; 
       betweenDirV[1] = (thumbDirV[1]*9 + indexDirV[1])/10; 
     }
-    const float betweenDirNorm = std::sqrt(std::pow(betweenDirV[0], 2) + std::pow(betweenDirV[1], 2));
-    const std::vector<float> normalizedBetweenDirV = {
+    const float betweenDirNorm = std::sqrt(std::pow(betweenDirV[0], 2) + std::pow(betweenDirV[1], 2)); // 中点ベクトルのノルム
+    const std::vector<float> normalizedBetweenDirV = { // 正規化された中点ベクトル
       float(betweenDirV[0]/betweenDirNorm),
       float(betweenDirV[1]/betweenDirNorm)};
-    printf("%f, %f, %f\n", normalizedBetweenDirV[0], normalizedBetweenDirV[1], std::pow(normalizedBetweenDirV[0], 2) + std::pow(normalizedBetweenDirV[1], 2));
+    printf("%f, %f, %f\n", normalizedBetweenDirV[0], normalizedBetweenDirV[1], std::pow(normalizedBetweenDirV[0], 2) + std::pow(normalizedBetweenDirV[1], 2)); // デバッグ用に中点位置を出力
 
 
     mediapipe::NormalizedRect handRect = packet3.Get<mediapipe::NormalizedRect>();
@@ -247,14 +260,22 @@ void overlayImage(cv::Mat* src, cv::Mat* overlay, const cv::Point& location) {
     // disabling annotation by deepcopying input frame. replace with output_frame_mat to re-enable
     cv::Mat input_frame_mat_copy = input_frame_mat.clone();
 
-    // display circle
+    // ウィンドウ内での手の基準点の位置
     float rectCenterX = handRect.x_center() * output_frame_mat.cols;
     float rectCenterY = handRect.y_center() * output_frame_mat.rows;
-    // weighted center of thumb and index
+    // 正規化されたピンチの位置（これは普通に中点）
     float pinchCenterNormX = (thumbTip.x()*1 + indexTip.x())/2;
     float pinchCenterNormY = (thumbTip.y()*1 + indexTip.y())/2;
+    
+    /*
+    pinchCenterNormとbetweenDirVの違い
+    pinchCenterNorm: どこを拡大するか。ちょうど中点。
+    betweenDirV: 拡大したものをどこに描画するか。重み付き平均。
+    */
+
     float pinchCenterX = (pinchCenterNormX) * output_frame_mat.cols;
     float pinchCenterY = (pinchCenterNormY) * output_frame_mat.rows;
+    // 握っていたらisPinching
     bool isPinching = 
       std::inner_product(indexBottomV.begin(), indexBottomV.end(), middleTipV.begin(), 0.0f) < 0 &&
       std::inner_product(indexBottomV.begin(), indexBottomV.end(), ringTipV.begin(), 0.0f) < 0 &&
@@ -267,8 +288,10 @@ void overlayImage(cv::Mat* src, cv::Mat* overlay, const cv::Point& location) {
       if (isPinching) {
         center.x = pinchCenterX;
         center.y = pinchCenterY;
-        // normalized with handrect.width()
+        // 親指と人差し指の距離。手の大きさで正規化することで手の大小にかかわらずピンチ状態を判定できる
         float distanceBetween = (std::pow(thumbTip.x() - indexTip.x(), 2) + std::pow(thumbTip.y() - indexTip.y(), 2)) / handRect.width() / handRect.width();
+
+        // 中点、距離についてaveNum個の平均をとる
         float distanceAverage = 0;
         float betweenVAverageX = 0;
         float betweenVAverageY = 0;
@@ -292,10 +315,11 @@ void overlayImage(cv::Mat* src, cv::Mat* overlay, const cv::Point& location) {
         recentBetweenVY[0] = normalizedBetweenDirV[1];
         betweenVAverageY += normalizedBetweenDirV[1];
         betweenVAverageY /= aveNum;
-        // printf("%f\n", distanceBetween);
+
         if (distanceBetween <= 0.01) {
           // trigger pinch image
           isActive = true;
+          // 検出失敗カウンタをリセット
           failCount = 0;
         }
         if (failCount <= 2) {
@@ -305,6 +329,8 @@ void overlayImage(cv::Mat* src, cv::Mat* overlay, const cv::Point& location) {
           float lensScale = distanceAverage/0.8*10 + 1;
           cv::resize(input_frame_mat, input_frame_mat, cv::Size(), lensScale, lensScale);
           cv::Rect ROI(cv::Point(lensScale*center.x - ROISize/2, lensScale*center.y - ROISize/2), cv::Size(ROISize, ROISize));
+
+          // クロップされた拡大映像が入る
           cv::Mat cropped = input_frame_mat(ROI);
 
           cv::Mat mask(cropped.size().width, cropped.size().height, CV_8UC1);//circle
@@ -314,24 +340,26 @@ void overlayImage(cv::Mat* src, cv::Mat* overlay, const cv::Point& location) {
           cv::Mat srcArray[] = {cropped, mask};
           int from_to[] = {0,0, 1,1, 2,2, 3,3};
           cv::mixChannels(srcArray, 2, &masked, 1, from_to, 4);
+          // ここまでで周囲が透明な円形ウィンドウができる
 
+          // 中点からずらされた描画位置
           cv::Point centerOffset;
           centerOffset.x = center.x - int(ROISize/2) + betweenVAverageX*int(ROISize/1.5);
           centerOffset.y = center.y - int(ROISize/2) + betweenVAverageY*int(ROISize/1.5);
-          overlayImage(&input_frame_mat_copy, &masked, centerOffset);
+          overlayImage(&input_frame_mat_copy, &masked, centerOffset); // 円形ウィンドウを描画
           centerOffset.x = center.x + betweenVAverageX*int(ROISize/1.5);
           centerOffset.y = center.y + betweenVAverageY*int(ROISize/1.5);
-          cv::circle(input_frame_mat_copy, centerOffset, cropped.size().width/2, cv::Scalar(255, 255, 255, 0.5), distanceAverage/0.8*3, cv::LINE_AA, 0);
-          cv::circle(input_frame_mat_copy, cv::Point(15, 22), 5, cv::Scalar(222, 137, 18), -1, cv::LINE_AA, 0);
-          cv::putText(input_frame_mat_copy, "Pinching", cv::Point(30, 30), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(200,200,200), 1, cv::LINE_AA);
+          cv::circle(input_frame_mat_copy, centerOffset, cropped.size().width/2, cv::Scalar(255, 255, 255, 0.5), distanceAverage/0.8*3, cv::LINE_AA, 0); // 白色の枠線を描画
+          cv::circle(input_frame_mat_copy, cv::Point(15, 22), 5, cv::Scalar(222, 137, 18), -1, cv::LINE_AA, 0); // 左上の青いインジケーター
+          cv::putText(input_frame_mat_copy, "Pinching", cv::Point(30, 30), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(200,200,200), 1, cv::LINE_AA); // 
         }
-      } else {
+      } else { // ピンチしていないとき
         isActive = false;
         failCount += 1;
-        cv::circle(input_frame_mat_copy, cv::Point(15, 22), 5, cv::Scalar(78, 184, 17), -1, cv::LINE_AA, 0);
-        cv::putText(input_frame_mat_copy, "Hand detected", cv::Point(30, 30), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(200,200,200), 1, cv::LINE_AA);
+        cv::circle(input_frame_mat_copy, cv::Point(15, 22), 5, cv::Scalar(78, 184, 17), -1, cv::LINE_AA, 0); // 左上の緑のインジケーター
+        cv::putText(input_frame_mat_copy, "Hand detected", cv::Point(30, 30), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(200,200,200), 1, cv::LINE_AA); // 左上のテキスト
       }
-    } else {
+    } else { // 手がないとき
       isActive = false;
       failCount += 1;
     }
